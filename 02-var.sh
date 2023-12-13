240f75b97f6f90caf0ecffc79ce66f457021fe45
chandu <chandu@devops.com>
2023-12-13 15:13:04 -0600

    added variabels

--- 02-var.sh
+++ 02-var.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+A=ramesh
+B=suresh
+echo "$A: hi $B gm"
+echo "$B: hi $A hi"
+echo "$A: how are you doing $B"
+echo "$B: i am doing good $A"
 echo "ramesh: hi suresh gm"
 echo "suresh: hi ramesh hi"
 echo "ramesh: how are you doing suresh"
