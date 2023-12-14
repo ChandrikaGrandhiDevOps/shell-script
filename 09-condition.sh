685b9e573fb1fdc9b67b7c51f0c09a1248221a35
chandu <chandu@devops.com>
2023-12-13 18:07:29 -0600

    modified message

--- 09-condition.sh
+++ 09-condition.sh
@@ -14,7 +14,16 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
    echo "Error: please run with root acces"
+   exit 1
 else
    echo "run"
 fi   
-yum install mysql -y   
+yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "ERROR:installing MYSQL is failed"
+    exit 1
+else
+    echo "installing mysql is sucesses"
+fi        
