6dde86c72208e75b073bc1f5bfed1183c469cea3
chandu <chandu@devops.com>
2023-12-13 23:23:47 -0600

    commites

--- 12-logs.sh
+++ 12-logs.sh
@@ -10,10 +10,10 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 VALIDATE(){
  if [ $1 -ne 0 ]
     then
-        echo "$R ERROR:: $2 FAILED"
+        echo -e "$R ERROR:: $2 FAILED"
         
     else
-        echo "$G  $2 i was installed it suceesesfully"
+        echo -e "$G  $2 i was installed it suceesesfully"
     fi
 }
 if [ $ID -ne 0 ]
@@ -21,7 +21,7 @@ then
     echo "ERROR:: FAILED"
     
 else
-    echo "$Y i was sucessfully installed"
+    echo -e "$Y i was sucessfully installed"
 fi
 yum install git -y &>> $LOGFILE
 VALIDATE  $? "git"
