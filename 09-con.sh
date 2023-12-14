60af6538666a078f7b06f7221d43f6e85c217d94
chandu <chandu@devops.com>
2023-12-13 21:05:34 -0600

    commites

--- 09-con.sh
+++ 09-con.sh
@@ -5,14 +5,16 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then 
     echo "ERROR: Please run this command from root access"
+    exit 2
 else
     echo "Sucess"
 fi 
 yum install mysql -y   
 
-if [ $ID -ne 0 ]
+if [ $? -ne 0 ]
 then
     echo "ERROR:mysql installation failed"
+    exit 2
 else
     echo "mysql sucessfully installled"
 fi        
