074a68e97b4de342c572701e895f74a18e15f137
chandu <chandu@devops.com>
2023-12-19 15:15:41 -0600

    done done

--- 17-diskusage.sh
+++ 17-diskusage.sh
@@ -9,9 +9,9 @@ do
    partition=$(echo  $line | awk '{print $1F}')
    if [ $usage -ge $DISK_THRESHOLD ]
       then
-    message+="High disk usage on $partition:$usage\n"
+    message+="High disk usage on $partition: $usage\n"
    fi    
 
 
 done <<< $DISK_USAGE
-echo "Message: $message"
+echo -e "Message: $message"
