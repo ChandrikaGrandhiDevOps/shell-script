3b181db06d2038f4e1bcf492bb3c973889292571
chandu <chandu@devops.com>
2023-12-19 15:08:37 -0600

    done done

--- 17-diskuage.sh
+++ 17-diskuage.sh
@@ -7,7 +7,7 @@ while IFS= read line
 do
 uage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
 partition=$(echo  $line | awk '{print $1F}')
-if [ $usage -gt $DISK_THRESHOLD ]
+if [ $usage -ge $DISK_THRESHOLD ]
 then
     message+="High disk usage on $partition:$usage"
 fi    
