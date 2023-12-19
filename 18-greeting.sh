d2480b5d6dc7d3ac3474b6517df1a2ea555ac8bb
chandu <chandu@devops.com>
2023-12-19 16:23:29 -0600

    done done

--- 18-greeting.sh
+++ 18-greeting.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-PERSON=$1
-echo "Hi $PERSON when you are learning shell script"
+NAME=""
+WISHES=""
+USAGE(){
+    echo "USAGE:: $(basename $0) -n <name> -w <wishes>"
+    echo "Options::"
+    echo "-n, Specify the name (mandatory)"
+    echo "-w, Specify the wishes (gm,gn,hru)"
+    echo "-h, Display help"
+}
+
+while getopts ":n:w:h" opt; do
+    case $opt in 
+         n) NAME="$OPTARG";;
+         w) WISHES="$OPTARG";;
+         h|*) USAGE; exit;;
+    esac      
+done
